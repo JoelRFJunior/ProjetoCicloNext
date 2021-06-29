a00c7341a75dd97b32600b991fd536481a0fe37b
Bruno <bruno_silth@hotmail.com>
2021-06-29 10:22:07 -0300

    criacao da model usuario e configuracao banco de dados

---
+++ ciclonext/src/main/java/com/ciclonext/ciclonext/model/Usuario.java
@@ -0,0 +1,62 @@
+package com.ciclonext.ciclonext.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotEmpty
+	private String nome;
+	
+	@NotEmpty
+	@Email
+	private String email;
+	
+	@NotEmpty
+	private String senha;
+
+	public long getId() {
+		return id;
+	
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
