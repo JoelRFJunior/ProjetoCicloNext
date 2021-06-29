f47712f0056bc9fcfdcbaf04aa263d7dbe93eb17
JoelRFJunior <joeljr_jc@hotmail.com>
2021-06-29 09:07:28 -0300

    construção spring do projeto ciclonext

---
+++ ciclonext/src/main/java/com/ciclonext/ciclonext/CiclonextApplication.java
@@ -0,0 +1,13 @@
+package com.ciclonext.ciclonext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiclonextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiclonextApplication.class, args);
+	}
+
+}
---
+++ ciclonext/src/test/java/com/ciclonext/ciclonext/CiclonextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclonext.ciclonext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiclonextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
