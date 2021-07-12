34610087e1462c3cd443ee26b549bf22196bb1d6
wendewfranco <wendewfranco@gmail.com>
2021-07-12 13:20:44 -0300

    Adicionando dados UsuarioLoginDTO

--- ciclonext/src/main/java/com/ciclonext/ciclonext/dtos/UsuarioLoginDTO.java
+++ ciclonext/src/main/java/com/ciclonext/ciclonext/dtos/UsuarioLoginDTO.java
@@ -1,5 +1,40 @@
 package com.ciclonext.ciclonext.dtos;
 
+import javax.validation.constraints.Email;
+
 public class UsuarioLoginDTO {
+	
+	@Email
+	private String email;
+	
+	private String senha;
+	
+	private String token;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
 
 }
