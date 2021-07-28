227aa63876dacc1c015548bdd965f794939d26eb
JoelRFJunior <joeljr_jc@hotmail.com>
2021-07-28 12:29:28 -0300

    implementando navbar, entrar,cadastrar, e rotas entre cadastrare entrar

--- angular/angularCiclonext/src/app/app.module.ts
+++ angular/angularCiclonext/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
