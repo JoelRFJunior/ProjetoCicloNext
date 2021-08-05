de5ae0061feae64587341046af547738fa85147d
JamiyM <jamilymelo787@gmail.com>
2021-08-05 14:45:43 -0300

    Implentando get e post de grupo

--- angular/angularCiclonext/src/app/model/Grupo.ts
+++ angular/angularCiclonext/src/app/model/Grupo.ts
@@ -5,6 +5,7 @@ export class Grupo {
 public idGrupo: number
 public nomeGrupo: string
 public descricao: string
+public urlImagemGrupo: string
 public postagens: Postagem[]
 public criador: Usuario
 }
